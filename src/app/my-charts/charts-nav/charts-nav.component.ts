28678a6a60a94198b5d6e607aa8383a8977093f1
nqbefgvs <389179622@qq.com>
2022-01-03 20:11:25 +0800

    charts-2-tax-1: knowledge preparation and text description

--- src/app/my-charts/charts-nav/charts-nav.component.ts
+++ src/app/my-charts/charts-nav/charts-nav.component.ts
@@ -1,22 +1,44 @@
-import {AfterViewInit, Component, ViewChild} from "@angular/core";
+import {AfterViewInit, Component, OnChanges, ViewChild} from "@angular/core";
+import {NavigationEnd, Router} from "@angular/router";
 
 @Component({
   selector: 'charts-nav',
   templateUrl: 'charts-nav.component.html',
   styleUrls: ['charts-nav.component.scss']
 })
-export class ChartsNavComponent implements AfterViewInit {
+export class ChartsNavComponent {
+
+  @ViewChild('myScrollContainer') myScrollContainer: any
 
   @ViewChild('myUl') myUl: any
 
-  public activated: number = 0
-
-  ngAfterViewInit(): void {
-    // this.myUl.nativeElement.children[this.activated].classList.add('activated')
+  constructor(private router: Router) {
+    router.events.subscribe((event) => {
+      if (event instanceof NavigationEnd) {
+        this.autoScrollWhenRoute()
+      }
+    })
   }
 
-  activate(event: MouseEvent): void {
+  wheelScrollContainer(event: WheelEvent): void {
+    event.preventDefault()
+    this.myScrollContainer.nativeElement.scrollBy({ left: +event.deltaY })
+  }
 
+  autoScrollWhenRoute(): void {
+    setTimeout(() => {
+      const container = this.myScrollContainer.nativeElement
+      const liList = [...this.myUl.nativeElement.children]
+      const curUrl = this.router.url.split('/').pop()
+      liList.forEach((elem: HTMLLIElement) => {
+        if (elem.children[0].getAttribute('routerLink') === curUrl) {
+          container.scrollTo({ left: elem.offsetLeft - container.clientWidth*0.5 + elem.clientWidth*0.5, behavior: 'smooth' })
+          elem.classList.add('activated')
+        } else {
+          elem.classList.remove('activated')
+        }
+      })
+    }, 0)
   }
 
 }
